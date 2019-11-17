4eed059abf46ac5193294aa74d672d7c017ebb31
Brandon Nguyen <z5205609@ad.unsw.edu.au>
2019-11-17 11:11:53 +1100

    rerout to mainscreen via DApp

--- src/unsw/application/DungeonApplication.java
+++ src/unsw/application/DungeonApplication.java
@@ -29,30 +29,25 @@ public class DungeonApplication extends Application {
         BufferScreen spLevel3Screen = new BufferScreen(primaryStage, "Level 3");
         spLevel3Screen.setNextLevel(spDungeon3);
 
+
+        
+        // Add returns to main menu
         BufferScreen gameFinished = new BufferScreen(primaryStage, "Congrats!");
         BufferScreen gameOver = new BufferScreen(primaryStage, "Game Over");
-        gameOver.setMainScreen(mainScreen);
+        gameFinished.setdApp(this);
+        gameOver.setdApp(this);
         spDungeon1.setGameOver(gameOver);
         spDungeon2.setGameOver(gameOver);
         spDungeon3.setGameOver(gameOver);
-        
-        
-
-
-        //InventoryScreen spInventoryScreen = new InventoryScreen(primaryStage);
-        
-        //GoalScreen goalScreen = new GoalScreen(primaryStage);
-        //DungeonScreen dungeonScreen1 = new DungeonScreen(primaryStage, "doorMaze.json");
-        //mainScreen.getController().setDungeonScreen(spDungeon1);
-        mainScreen.getController().setSinglePlayerScreen(spLevel1Screen);
-        mainScreen.getController().setMainScreen(mainScreen);
         
         // Connect the dungeons to the next screen
         spDungeon1.setNextScreen(spLevel2Screen); 
         spDungeon2.setNextScreen(spLevel3Screen);
         spDungeon3.setNextScreen(gameFinished);
-        gameFinished.setMainScreen(mainScreen);
         
+        mainScreen.getController().setSinglePlayerScreen(spLevel1Screen);
+        mainScreen.getController().setMainScreen(mainScreen);
+            
         mainScreen.start();    
     }
 
